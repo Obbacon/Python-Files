926125544c498a32d96f59d12aa6bf80efa034f9
obbacon <rayx70@live.ca>
2016-05-30 12:22:59 -0400

    Just a few changes

--- Test 2.py
+++ Test 2.py
@@ -1,18 +1,26 @@
 # First Solution
-import random
-
-number = random.randrange(0, 19)
-guess = int(input("Enter an integer between 1 and 20: "))
-
-while guess != number:
-    if guess == number:
-        print("You guessed correctly!")
-        continue
-    elif guess < number:
-        print("Too low yo")
-        continue
-    elif guess > number:
-        print("Too high guy")
-        continue
-
+"""
+num = int(input("Enter a number: "))
+mod = num % 2
+if mod > 0:
+    print("You picked an odd number.")
+else:
+    print("You picked an even number.")
+"""
 # Second Solution
+
+check = int(input("Give me a number to check: "))
+num = int(input("Give me a number a divide by: "))
+
+if num % 4 == 0:
+    print(num, "is a multiple of 4")
+elif num % 2 == 0:
+    print(num, "is an even number")
+else:
+    print(num, "is an odd number")
+
+if num % check == 0:
+    print(num, "divides evenly by", check)
+else:
+    print(num, "does not divide evenly by", check)
+
