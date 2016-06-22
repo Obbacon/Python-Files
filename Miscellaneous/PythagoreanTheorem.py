e63fd672d79fd7e5c092a3eb98a86f646b4eee37
obbacon <rayx70@live.ca>
2016-06-22 08:45:07 -0400

    Just a few changes

---
+++ Miscellaneous/PythagoreanTheorem.py
@@ -0,0 +1,8 @@
+a = float(input("Enter the length of the first side: "))
+b = float(input("Enter the length of the second side: "))
+
+c_squared = a**2 + b**2
+
+c = c_squared**0.5
+print("The length of the hypotenuse is", c)
+
---
+++ Miscellaneous/ShoppingListProgram.py
@@ -0,0 +1,12 @@
+current_item = " "
+item = []
+
+
+while current_item != "":
+    current_item = input("Enter an item: ")
+    if current_item != "":
+        item.append(current_item)
+
+print(item)
+
+
